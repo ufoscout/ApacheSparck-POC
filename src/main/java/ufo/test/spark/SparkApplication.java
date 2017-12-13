f37cbb19246eeb598542349b271b42a5c98f1275
ufoscout <ufoscout@gmail.com>
2017-12-13 18:37:58 +0100

    Clean

--- src/main/java/ufo/test/spark/SparkApplication.java
+++ src/main/java/ufo/test/spark/SparkApplication.java
@@ -2,10 +2,8 @@ package ufo.test.spark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class SparkApplication {
 
     public static void main(String[] args) {
